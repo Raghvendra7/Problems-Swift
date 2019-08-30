244a49420554ab34e5bcb21ffe9d838672684338
Raghvendra Srivastava <raghv77@gmail.com>
2019-08-30 13:11:06 +0530

    Create checkMagazine.swift

---
+++ checkMagazine.swift
@@ -0,0 +1,29 @@
+
+//https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+
+
+func checkMagazine(magazine: [String], note: [String]) -> Void {
+    if magazine.count < note.count{
+        print("No")
+    }
+    var dict = [String:Int]()
+    for data in magazine{
+        let val = dict[data] ?? 0
+        dict[data] = val + 1
+    }
+    for data in note{
+        if let val = dict[data]{
+            if val == 0{
+                print("No")
+                return
+            }
+            dict[data] = val - 1
+        }else{
+            print("No")
+            return
+        }
+      
+    }
+        print("Yes")
+}
